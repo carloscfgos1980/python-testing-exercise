989c8a35ed41296502aa641d57fc32e4a7f7c770
Carlos Infante <kuva5008@gmail.com>
2023-01-03 09:40:40 +0100

    problem with dictionary inside list

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 def get_none():
     return None
+
+
+def flatten_dict(book):
+    for key, value in book.items():
+        if type(value) == dict:
+            print('key', key)
+            print('value', value)
+            c = list(value.values())
+            return c
+            # Check if is a Dictionary inside a list
+        elif type(value) == list:
+            print('list-value:', value)  # checking the value
+            x = str(value)  # turning the list into a string
+            # slicing the string to isolate the number
+            x = x[x.find(' ') + 1:-2]
+            x = [int(x)]  # turning the string into a number
+            return x
+        else:
+            d = list(book.values())
+            return d
--- test_main.py
+++ test_main.py
@@ -5,29 +5,20 @@ def test_get_none():
     assert main.get_none() == None
 
 
-def flatten_dict(book):
-    outcome = list(book.values())
-    print(outcome)
-    return outcome
-
-
-print(flatten_dict({'a': [42, 350], 'b': 3.14}))
-
-
 def test_flatten_dict():
-    assert type(flatten_dict({'a': 42, 'b': 3.14})) == list
-    assert flatten_dict({'a': 42, 'b': 3.14}) == [42, 3.14]
-    assert type(flatten_dict({'a': [42, 350], 'b': 3.14})) == list
-    assert flatten_dict({'a': [42, 350], 'b': 3.14}) == [[42, 350], 3.14]
-    assert type(flatten_dict(
+    assert type(main.flatten_dict({'a': 42, 'b': 3.14})) == list
+    assert main.flatten_dict({'a': 42, 'b': 3.14}) == [42, 3.14]
+    assert type(main.flatten_dict({'a': [42, 350], 'b': 3.14})) == list
+    assert main.flatten_dict({'a': [42, 350], 'b': 3.14}) == [[42, 350], 3.14]
+    assert type(main.flatten_dict(
         {'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14})) == list
-    assert flatten_dict(
-        {'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14}) == [{'inner_a': 42, 'inner_b': 350}, 3.14]
+    # assert main.flatten_dict(
+    # {'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14}) == [{'inner_a': 42, 'inner_b': 350}, 3.14]
 
-    assert type(flatten_dict(
+    assert type(main.flatten_dict(
         {'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14})) == list
-    assert flatten_dict(
+    assert main.flatten_dict(
         {'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14}) == [42, 350, 3.14]
 
-    assert type(flatten_dict({'a': [{'inner_inner_a': 42}]})) == list
-    assert flatten_dict({'a': [{'inner_inner_a': 42}]}) == [42]
+    assert type(main.flatten_dict({'a': [{'inner_inner_a': 42}]})) == list
+    assert main.flatten_dict({'a': [{'inner_inner_a': 42}]}) == [42]
